58b8859d675ca9a6f40d902ed833011c7839c9c8
NitishGoswami-28 <158745401+NitishGoswami-28@users.noreply.github.com>
2024-02-03 18:33:30 +0530

    Delete sort_by2nd.py

--- sort_by2nd.py
+++
@@ -1,6 +0,0 @@
-tuple_list = [(1, 5), (3, 2), (2, 8), (4, 3)]
-
-sorted_tuples = sorted(tuple_list, key=lambda x: x[1])
-
-print("Original tuple list:", tuple_list)
-print("Sorted tuples by second item:", sorted_tuples)
