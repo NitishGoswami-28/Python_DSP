517a94fe02867c91019592cd8167d1f0a39e7f51
NitishGoswami-28 <158745401+NitishGoswami-28@users.noreply.github.com>
2024-02-03 18:33:12 +0530

    Delete rem_tup.py

--- rem_tup.py
+++
@@ -1,7 +0,0 @@
-
-tuple_list = [(1, 2, 3), ('a', 'b'), (4, 5, 6), ('x', 'y', 'z')]
-k_len = 3
-tuples =[tpl for tpl in tuple_list if len(tpl) != k_len]
-
-print("Original tuple list:", tuple_list)
-print(f"Tuples of length {k_len} removed:", tuples)
